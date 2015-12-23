332a497aceedef2d790b8034d920583df8d88705
Nathan Sollenberger <nathan@sgizmo.com>
2015-12-23 14:32:35 -0700

    Clean up notify.py a bit

--- server/html/cgibin/notify.py
+++ server/html/cgibin/notify.py
@@ -3,22 +3,16 @@
 import os
 import urlparse
 
-import uc
-
 # CGI scripts must output header manually
 print "Content-type: text/html"
 print "Access-Control-Allow-Origin: *"
 print
 # Content follows
-print "<h1>Hello world</h1>"
-
-# Example of calling function in uc module
-uc.meow("But really though")
+print "Ok"
 
 # Grab query string using os and urlparse
 query = os.environ[ "QUERY_STRING" ]
 parsed = urlparse.parse_qs(query)
 for key, value in parsed.items():
-      print "<paragraph>You set '%s' to value %s</paragraph>" % \
-         ( key, value )
+    print "<p>'%s' = %s</p>" % (key, value)
 
--- server/html/cgibin/uc.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-def meow(test):
-    print '<p style="font-style:italic">~' + test + '</p>'
