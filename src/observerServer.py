d74472a70e228adc2849be0961fea29355f861a1
Nathan Sollenberger <nathan@spacenate.com>
2016-01-10 14:50:01 -0700

    Switch to pyUSB from cython-hidapi

--- src/observerServer.py
+++ src/observerServer.py
@@ -2,34 +2,51 @@
 from flask import Flask, request, jsonify
 from flask.ext.cors import CORS
 import ssl
-import hid
+import usb.core
 
-VENDOR_ID = 0x05DF
-PRODUCT_ID = 0x16C0
+# bmRequestType USB standard bit masks - Type
+USB_TYPE_STANDARD = (0x00 << 5)
+USB_TYPE_CLASS = (0x01 << 5)
+USB_TYPE_VENDOR = (0x02 << 5)
+USB_TYPE_RESERVED = (0x03 << 5)
+# bmRequestType USB standard bit masks - Recipient
+USB_RECIP_DEVICE = 0x00
+USB_RECIP_INTERFACE = 0x01
+USB_RECIP_ENDPOINT = 0x02
+USB_RECIP_OTHER = 0x03
+# bmRequestType USB standard bit masks - Endpoint
+USB_ENDPOINT_IN = 0x80
+USB_ENDPOINT_OUT = 0x00
+# Custom bRequest byte values
+CUSTOM_RQ_STATUS_OFF = 0x00
+CUSTOM_RQ_STATUS_AVAIL = 0x01
+CUSTOM_RQ_STATUS_UNAVAIL = 0x02
+CUSTOM_RQ_STATUS_MAXCHATS = 0x03
+CUSTOM_RQ_STATUS_UNREAD = 0x04
+CUSTOM_RQ_CONFIRM = 0x01
+# OlarkObserver device details
+DEVICE_VENDOR_ID = 0x05DF
+DEVICE_PRODUCT_ID = 0x16C0
 MANUFACTURER_STR = 'Spacenate.com'
 PRODUCT_STR = 'OlarkObserver'
-SET = 0x00
-CONFIRM = 0x01
-STATUS_OFF = 0xFF
-STATUS_AVAIL = 0x00
-STATUS_UNAVAIL = 0x01
-STATUS_MAXCHATS = 0x02
-STATUS_UNREAD = 0x03
+TRANSFER_LENGTH = 2
 
-def messageDevice(message):
-    h = hid.device()
-    h.open(VENDOR_ID, PRODUCT_ID)
-    manufacturer = h.get_manufacturer_string()
-    product = h.get_product_string()
-    message = [0x00] + message # endpoint id 0
-    sent = h.send_feature_report(message)
-    deviceResponse = h.read(2, 100)
-    h.close()
-    if manufacturer != MANUFACTURER_STR or product != PRODUCT_STR:
-        raise IOError('manufacturer or product string mismatch')
-    if sent != len(message):
-        raise IOError('usb communication error')
-    return deviceResponse
+class USBDevice:
+    def __init__(self, vendor=DEVICE_VENDOR_ID, product=DEVICE_PRODUCT_ID):
+        self.device = usb.core.find(idVendor=vendor, idProduct=product)
+        if self.device is None:
+            raise IOError('USB device not found')
+        if (self.device.product != PRODUCT_STR or
+              self.device.manufacturer != MANUFACTURER_STR):
+            raise IOError('Manufacturer or product string mismatch')
+        self.device.set_configuration()
+
+    def set_status(self, status, wValue = 0x0000, wIndex = 0x0000):
+        response = self.device.ctrl_transfer(bmRequestType=(USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN),
+                                             bRequest=status, wValue=wValue, wIndex=wIndex, data_or_wLength=TRANSFER_LENGTH)
+        if len(response) != TRANSFER_LENGTH:
+            raise IOError('USB communication error')
+        return response
 
 app = Flask(__name__)
 CORS(app)
@@ -41,11 +58,12 @@ def index():
 @app.route('/device', methods=['GET'])
 def checkDeviceStatus():
     try:
-        deviceResponse = messageDevice([SET, STATUS_OFF])
+        device = USBDevice()
+        response = device.set_status(CUSTOM_RQ_STATUS_OFF)
     except IOError, ex:
         return jsonify(result='NAK', message=ex.message)
-    if deviceResponse != [CONFIRM, STATUS_OFF]:
-        return jsonify(result='NAK', message='invalid device response: '+','.join(deviceResponse))
+    if response[0] != CUSTOM_RQ_CONFIRM:
+        return jsonify(result='NAK', message='invalid device response: '+str(response))
     return jsonify(result='ACK')
 
 @app.route('/status/<status>', methods=['PUT', 'GET'])
@@ -53,28 +71,30 @@ def updateStatus(status):
     withChats = request.args.get('withChats', '')
     # todo - figure out possible statuses
     # and messages to change to each status
-    newStatus = STATUS_AVAIL
+    newStatus = CUSTOM_RQ_STATUS_AVAIL
     try:
-        deviceResponse = messageDevice([SET, newStatus])
+        device = USBDevice()
+        response = device.set_status(newStatus)
     except IOError, ex:
         return jsonify(result='NAK', message=ex.message)
-    if deviceResponse != [CONFIRM, newStatus]:
-        return jsonify(result='NAK', message='invalid device response: '+deviceResponse)
+    if response[0] != CUSTOM_RQ_CONFIRM:
+        return jsonify(result='NAK', message='invalid device response: '+str(response))
     return jsonify(result='ACK')
 
-@app.route('/chats/<tabName>/<int:difference>', methods=['PUT'])
+@app.route('/chats/<tabName>/<int:difference>', methods=['PUT','GET'])
 def updateChats(tabName, difference):
-    # 'new' if new tab
-    # otherwise, updating existing chat tabs
+    # if tabName == 'new': # New chat session
+    # else: # Updating an existing chat session
     # 1 added a chat
     # 0 removed a chat
-    newStatus = STATUS_UNREAD
+    newStatus = CUSTOM_RQ_STATUS_UNREAD
     try:
-        deviceResponse = messageDevice([SET, newStatus])
+        device = USBDevice()
+        response = device.set_status(newStatus)
     except IOError, ex:
         return jsonify(result='NAK', message=ex.message)
-    if deviceResponse != [CONFIRM, newStatus]:
-        return jsonify(result='NAK', message='invalid device response: '+deviceResponse)
+    if response[0] != CUSTOM_RQ_CONFIRM:
+        return jsonify(result='NAK', message='invalid device response: '+str(response))
     return jsonify(result='ACK')
 
 if __name__ == '__main__':
