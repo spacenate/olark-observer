5128468bb9d16d4c87d7447be68a5ce301393b97
Nathan Sollenberger <nathan@spacenate.com>
2016-01-10 18:05:32 -0700

    Add license

--- observer_server/setup.py
+++ observer_server/setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="Olark Observer",
+    name="Observer Server",
     version="0.1",
-
     author="Nathan Sollenberger",
     author_email="nathan@spacenate.com",
     url="https://github.com/spacenate/olark-observer",
     description="Relay Olark operator updates to an embedded system via USB.",
-    license="LICENSE.txt",
-
+    license="MIT",
     packages=["observer_server"],
     include_package_data=True,
-
     install_requires=[
         "flask>=0.10",
         "flask-cors",
